6b55024302ccb7abeac6d19f8cfe9a89a0cad0aa
manu mendez <manumendez@mpg.io>
2022-06-20 20:17:42 +0200

    Added player jump and a barrel that creates a radial force push when hit with a projectile

---
+++ Source/ActionRoguelike/Private/SExplosiveBarrel.cpp
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ActionRoguelike/Public/SExplosiveBarrel.h"
+#include "PhysicsEngine/RadialForceComponent.h"
+#include "Components/StaticMeshComponent.h"
+
+
+// Sets default values
+ASExplosiveBarrel::ASExplosiveBarrel()
+{
+	// Create the mesh component, set the collission profile and make it our root component
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>("MeshComp");
+	MeshComp->SetSimulatePhysics(true);
+	// Enabling Simulate physics automatically changes the Profile to PhysicsActor in Blueprint, in C++ we need to change this manually.
+	MeshComp->SetCollisionProfileName(UCollisionProfile::PhysicsActor_ProfileName); 
+	RootComponent = MeshComp;
+
+	ForceComp = CreateDefaultSubobject<URadialForceComponent>("ForceComp");
+	ForceComp->SetupAttachment(MeshComp);
+
+	// Leaving this on applies small constant force via component 'tick' (Optional)
+	ForceComp->SetAutoActivate(false);
+
+	ForceComp->Radius = 750.0f;
+	ForceComp->ImpulseStrength = 2500.0f; // Alternative: 200000.0 if bImpulseVelChange = false
+	// Optional, ignores 'Mass' of other objects (if false, the impulse strength will be much higher to push most objects depending on Mass)
+	ForceComp->bImpulseVelChange = true;
+
+	// Optional, default constructor of component already adds 4 object types to affect, excluding WorldDynamic
+	ForceComp->AddCollisionChannelToAffect(ECC_WorldDynamic);
+
+	MeshComp->OnComponentHit.AddDynamic(this, &ASExplosiveBarrel::OnActorHit);
+}
+
+// Called when the game starts or when spawned
+void ASExplosiveBarrel::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ASExplosiveBarrel::OnActorHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	ForceComp->FireImpulse();
+}
+
---
+++ Source/ActionRoguelike/Public/SExplosiveBarrel.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "SExplosiveBarrel.generated.h"
+
+class UStaticMeshComponent;
+class URadialForceComponent;
+
+UCLASS()
+class ACTIONROGUELIKE_API ASExplosiveBarrel : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ASExplosiveBarrel();
+
+protected:
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* MeshComp;
+
+	UPROPERTY(VisibleAnywhere)
+	URadialForceComponent* ForceComp;
+	
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Must be marked with ufunction in order to 'bind' the event
+	UFUNCTION()
+	void OnActorHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+	
+};
+ 
